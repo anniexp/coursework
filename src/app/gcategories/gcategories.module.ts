bc207816c66637e6cc3eb11622c55b904ba14534
anniexp <59566543+anniexp@users.noreply.github.com>
2021-06-19 02:51:24 +0300

    category-edit-dialog comp
    for module edit

--- src/app/gcategories/gcategories.module.ts
+++ src/app/gcategories/gcategories.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { GcategoriesRoutingModule } from './gcategories-routing.module';
 import { GcategoriesListComponent } from './components/gcategories-list/gcategories-list.component';
+import { CategoryEditDialogComponent } from './components/category-edit-dialog/category-edit-dialog.component';
 
 
 @NgModule({
   declarations: [
-    GcategoriesListComponent
+    GcategoriesListComponent,
+    CategoryEditDialogComponent
   ],
   imports: [
     CommonModule,
