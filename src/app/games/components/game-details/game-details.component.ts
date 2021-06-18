466a638a70e4760828425472e626d49437b62b20
anniexp <59566543+anniexp@users.noreply.github.com>
2021-06-18 22:25:40 +0300

    private route to game added

--- src/app/games/components/game-details/game-details.component.ts
+++ src/app/games/components/game-details/game-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'lq-game-details',
@@ -6,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-details.component.scss']
 })
 export class GameDetailsComponent implements OnInit {
-
-  constructor() { }
+//ActivatedRoute pazi informacia za koi component e zareden v momenta, skriti danni se podavat
+  constructor(private route: ActivatedRoute) { 
+//snapshotva tekushto id xd
+    console.log(this.route.snapshot.paramMap.get('id'));
+  }
 
   ngOnInit(): void {
   }
